b5aab499546818bd4fba21c9e52618b1bc7f6e47
Dimpal Pariyar <137203598+DimpalPariyar@users.noreply.github.com>
2024-06-19 16:41:18 +0530

    Update Cart.jsx

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
-// import CartItem from "./CartItem";
+import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart } from "./cartSlice";
 import EmptyCart from "./EmptyCart";
