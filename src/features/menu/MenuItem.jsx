ed51cc84fedcf779a53cea0616d614005ae7080b
Dimpal Pariyar <137203598+DimpalPariyar@users.noreply.github.com>
2024-06-19 17:30:24 +0530

    Update MenuItem.jsx

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,7 +3,7 @@ import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
-import UpdateItemQuantity from "../cart/updateItemQuantity";
+// import UpdateItemQuantity from "../cart/updateItemQuantity";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -48,10 +48,10 @@ function MenuItem({ pizza }) {
 
           {isInCart && (
             <div className="flex item-center gap-3 sm:gap-8">
-              <UpdateItemQuantity
+{/*               <UpdateItemQuantity
                 pizzaId={id}
                 currentQuantity={currentQuantity}
-              />
+              /> */}
               <DeleteItem pizzaId={id} />
             </div>
           )}
